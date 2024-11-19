41c9704a4a6edec57bf883687f28fa0a40236623
Harrison <harrison.oreibow@gmail.com>
2024-11-20 08:17:13 +1300

    basic level editor functionality

--- level_editor.py
+++ level_editor.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import Settings
+import tiles
 
 
 class Game:
@@ -10,11 +11,11 @@ class Game:
         self.dt = 0
         self.running = True
 
-        self.scroll_speed = 1000
-        self.scroll = pygame.Vector2(50, 50)
+        self.grid_cell_size = 70
+        self.grid_dimensions = (16, 9)
 
-        self.grid_square_size = 70
-        self.grid_size = (16, 9)
+        self.level: set[tiles.Tile] = set()
+        self.current_tile_type = 0
 
     def scene(self, dt: float) -> None:
         for event in pygame.event.get():
@@ -23,28 +24,36 @@ class Game:
                 return
 
         self.screen.fill((10, 200, 80))
-        self.handle_scroll(dt)
         self.draw_grid()
-
-    def handle_scroll(self, dt) -> None:
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP]:
-            self.scroll.y += self.scroll_speed * dt
-        if keys[pygame.K_DOWN]:
-            self.scroll.y -= self.scroll_speed * dt
-        if keys[pygame.K_LEFT]:
-            self.scroll.x += self.scroll_speed * dt
-        if keys[pygame.K_RIGHT]:
-            self.scroll.x -= self.scroll_speed * dt
+        self.handle_place()
+        self.draw_tiles()
 
     def draw_grid(self) -> None:
-        for x in range(self.grid_size[0]):
-            pygame.draw.line(self.screen, "black", (x*self.grid_square_size + self.scroll.x, self.scroll.y),
-                             (x*self.grid_square_size + self.scroll.x, self.grid_square_size * (self.grid_size[1] - 1) + self.scroll.y))
-        for y in range(self.grid_size[1]):
-            pygame.draw.line(self.screen, "black", (self.scroll.x, y*self.grid_square_size + self.scroll.y),
-                             (self.grid_square_size * (self.grid_size[0] - 1) + self.scroll.x, y*self.grid_square_size + self.scroll.y))
+        for x in range(self.grid_dimensions[0]):
+            pygame.draw.line(self.screen, "black", (x*self.grid_cell_size, 0),
+                             (x*self.grid_cell_size, self.grid_cell_size * (self.grid_dimensions[1] - 1)))
+        for y in range(self.grid_dimensions[1]):
+            pygame.draw.line(self.screen, "black", (0, y*self.grid_cell_size),
+                             (self.grid_cell_size * (self.grid_dimensions[0] - 1), y*self.grid_cell_size))
+
+    def draw_tiles(self) -> None:
+        for tile in self.level:
+            tile.draw(self.screen)
+
+    def handle_place(self) -> None:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_1]:
+            self.current_tile_type = 0
+        elif keys[pygame.K_2]:
+            self.current_tile_type = 1
+
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_button = pygame.mouse.get_pressed()[0]
+        if mouse_button:
+            cell_x = mouse_pos[0] // self.grid_cell_size
+            cell_y = mouse_pos[1] // self.grid_cell_size
+            self.level.add(tiles.tiles[self.current_tile_type](
+                cell_x * self.grid_cell_size, cell_y * self.grid_cell_size, self.grid_cell_size))
 
     def loop(self) -> None:
         self.dt = self.clock.tick_busy_loop(self.settings.max_fps) / 1000
---
+++ tiles.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+class Tile:
+    def __init__(self, x: float, y: float, size: float, colour: tuple[int, int, int]) -> None:
+        self.pos = pygame.Vector2(x, y)
+        self.image = pygame.Surface((size, size))
+        self.image.fill(colour)
+
+    def draw(self, surface: pygame.Surface) -> None:
+        surface.blit(self.image, self.pos)
+
+    # used for determining unique instances for use in sets
+    def __hash__(self) -> int:
+        return hash((self.pos.x, self.pos.y))
+
+    def __eq__(self, other) -> bool:
+        return self.pos == other.pos
+
+
+class WallTile(Tile):
+    def __init__(self, x: float, y: float, size: float) -> None:
+        super().__init__(x, y, size, (100, 100, 100))
+
+
+class LavaTile(Tile):
+    def __init__(self, x: float, y: float, size: float) -> None:
+        super().__init__(x, y, size, (200, 50, 50))
+
+
+tiles = (WallTile, LavaTile)
+
+if __name__ == "__main__":
+    level: set[Tile] = set()
+    level.add(WallTile(0, 0, 50))
+    level.add(LavaTile(50, 50, 50))
+    level.add(WallTile(0, 0, 50))
+    print(level)
