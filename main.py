8b94463868e58af02299ed012b7a692dff02e889
Harrison O'Reibow <harrison.oreibow@gmail.com>
2025-07-09 13:31:58 +1200

    Improve different sized window handling

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
-from settings import Settings
-from player import Player
+
 import tiles
+from player import Player
+from settings import Settings
 
 
 class Game:
     def __init__(self) -> None:
-        self.settings = Settings()
+        self.settings = Settings(
+            resolution=(1280, 720), max_fps=240, fullscreen=False
+        )
         self.screen = self.settings.screen
         self.subscreen = pygame.Surface((1920, 1080))
         self.dt: float = 0
@@ -39,7 +42,32 @@ class Game:
 
         self.scene(self.dt)
 
-        pygame.transform.scale(self.subscreen, self.settings.resolution, self.screen)
+        draw_resolution = pygame.Vector2()
+        window_aspect_ratio = (
+            self.settings.resolution[0] / self.settings.resolution[1]
+        )
+        subscreen_aspect_ratio = (
+            self.subscreen.get_size()[0] / self.subscreen.get_size()[1]
+        )
+        if subscreen_aspect_ratio > window_aspect_ratio:
+            draw_resolution.update(
+                self.settings.resolution[0],
+                self.settings.resolution[0] * (9 / 16),
+            )
+        else:
+            draw_resolution.update(
+                self.settings.resolution[1] * (16 / 9),
+                self.settings.resolution[1],
+            )
+
+        self.screen.blit(
+            pygame.transform.scale(self.subscreen, draw_resolution),
+            (
+                (self.settings.resolution[0] - draw_resolution.x) / 2,
+                (self.settings.resolution[1] - draw_resolution.y) / 2,
+            ),
+        )
+
         pygame.display.flip()
 
 
